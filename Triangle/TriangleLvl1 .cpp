fd9f7e95c71ba0f0a05e4233ee18cc44843d2eaa
AerKealy <alirizabeyhan@outlook.com>
2024-11-07 20:13:05 +0300

    Triangle Function

---
+++ Triangle/TriangleLvl1 .cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int number;
+    cout << "Please Enter The Lenght Of The Triangle: ";
+    cin >> number;
+
+    for(int j = 1; j <= number; j++ ){
+        
+        for(int h =1; h <= j; h++ ){
+            cout << "*";
+        }
+
+        cout << '\n';
+
+    }
+
+    return main();
+}
