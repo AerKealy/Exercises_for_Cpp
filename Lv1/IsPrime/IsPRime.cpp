27ba9fca9a342267e62f5e85ed6b3ad319c8ae49
AerKealy <alirizabeyhan@outlook.com>
2024-12-12 14:04:51 +0300

    Added

---
+++ Lv1/IsPrime/IsPRime.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int number;
+    bool Prime = 1;
+
+    do{
+        cout << "Enter a number [2-100]: ";
+        cin >> number;
+    }while(number<2 || number > 100);
+
+    for(int i=2; i<number; i++){
+        if(number % i == 0){
+            Prime = 0;
+            break;
+        }
+        else{
+            Prime = 1;
+        }
+    }
+
+    if(Prime)
+        cout << "Number is prime";
+    else
+        cout << "Number is not prime";
+    return 0;
+}
