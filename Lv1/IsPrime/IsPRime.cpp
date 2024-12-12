9b32a585684dea3cf81c2faff0ed8ca5cea19624
AerKealy <alirizabeyhan@outlook.com>
2024-12-12 14:07:06 +0300

    Small Change

--- Lv1/IsPrime/IsPRime.cpp
+++ Lv1/IsPrime/IsPRime.cpp
@@ -11,7 +11,7 @@ int main(){
         cin >> number;
     }while(number<2 || number > 100);
 
-    for(int i=2; i<number; i++){
+    for(int i=2; i<number; i++){ // If i is = 0 then program will divide bt 0 and fail
         if(number % i == 0){
             Prime = 0;
             break;
